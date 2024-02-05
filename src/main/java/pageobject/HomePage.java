30111e761538012b77b54331847874cc962aefe5
Natalia Pototskaia <kusochek-91@mail.ru>
2024-02-05 21:48:23 +0300

    new project for sprint 4

--- src/main/java/pageobject/HomePage.java
+++ src/main/java/pageobject/HomePage.java
@@ -1,2 +1,55 @@
-package pageobject;public class HomePage {
+package pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class HomePage {
+    private final WebDriver driver;
+    private final By orderTop = By.className("Button_Button__ra12g"); //кнопка Заказать вверху
+    public final By orderDown = By.xpath("//div[@class='Home_FinishButton__1_cWm']/button");//кнопка Заказать внизу
+    private final By answers = By.xpath(".//div[@class='accordion__panel']//*");//ответы
+    private final By textAboutImportant = By.xpath(".//div[@class='accordion__panel']//*"); //текст ответов
+    private final By headerAboutImportant = By.className("Home_FourPart__1uthg");//весь блок вопросов о важном
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void waitForLoadAccordionData() {
+        new WebDriverWait(driver,  Duration.ofSeconds(30)).until(driver -> (driver.findElement(textAboutImportant).getText() != null
+        ));
+    }
+
+    public String checkText(String textAboutImportantQuestion, int counter) {
+        String textAnswers = null;
+        final String questionOptionTemplate = ".//div[@class='accordion']//*[text()='" + textAboutImportantQuestion + "']";
+        driver.findElement(By.xpath(questionOptionTemplate)).click();
+        List<WebElement> answersList = driver.findElements(answers);
+        waitForLoadAccordionData();
+        for (int i = 0; i <= answersList.size(); i++) {
+            textAnswers = answersList.get(counter).getText();
+        }
+        return textAnswers;
+    }
+
+    public void waitForLoadProfileData() {
+        new WebDriverWait(driver,  Duration.ofSeconds(30)).until(driver -> (driver.findElement(headerAboutImportant).getText() != null
+                && !driver.findElement(headerAboutImportant).getText().isEmpty()
+        ));
+    }
+
+
+    public void clickOrderDownButton() {
+        driver.findElement(orderDown).click();
+    }
+
+    public void clickOrderTopButton() {
+        driver.findElement(orderTop).click();
+    }
+
 }
--- src/test/java/RunTestQuestions.java
+++ src/test/java/RunTestQuestions.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class RunTestQuestions {
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pageobject.HomePage;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class RunTestQuestions {
+    private final String checkedText;
+    private final int index;
+    private final String expected;
+    private final By headerAboutImportantField = By.className("Home_FAQ__3uVm4");
+    private final By cookieButton = By.id("rcc-confirm-button");
+    WebDriver driver;
+
+    public RunTestQuestions(String checkedText, int index, String expected) {
+        this.checkedText = checkedText;
+        this.index = index;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {"Сколько это стоит? И как оплатить?", 0, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {"Хочу сразу несколько самокатов! Так можно?", 1, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {"Как рассчитывается время аренды?", 2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {"Можно ли заказать самокат прямо на сегодня?", 3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {"Можно ли продлить заказ или вернуть самокат раньше?", 4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {"Вы привозите зарядку вместе с самокатом?", 5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {"Можно ли отменить заказ?", 6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {"Я жизу за МКАДом, привезёте?", 7, "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
+        };
+    }
+
+
+    @Before
+    public void startTest() {
+       WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        driver.findElement(cookieButton).click();
+        WebElement element = driver.findElement(headerAboutImportantField);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+
+    }
+
+    @Test
+    public void shouldCheckAnswersTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.waitForLoadProfileData();
+        assertEquals(expected, homePage.checkText(checkedText, index));
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
 }
